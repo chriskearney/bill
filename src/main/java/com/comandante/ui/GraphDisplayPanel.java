c10768750058fdd3c684b7b3a74513c6c777b649
Chris Kearney <chris@kearneymail.com>
2014-11-30 21:50:14 -0800

    initial checkin

---
+++ src/main/java/com/comandante/ui/GraphDisplayPanel.java
@@ -0,0 +1,24 @@
+package com.comandante.ui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GraphDisplayPanel extends JPanel {
+
+    private final Image img;
+
+    public GraphDisplayPanel(Image img) {
+        this.img = img;
+        Dimension size = new Dimension(img.getWidth(null), img.getHeight(null));
+        setPreferredSize(size);
+        setMinimumSize(size);
+        setMaximumSize(size);
+        setSize(size);
+        setLayout(null);
+    }
+
+    public void paintComponent(Graphics g) {
+        g.drawImage(img, 0, 0, null);
+    }
+
+}
