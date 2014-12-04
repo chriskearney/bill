8ccb396e20147a19280f7f46ae2ef5754cae47aa
Chris Kearney <chris@kearneymail.com>
2014-12-03 23:05:39 -0800

    crude in some ways, but it works.

--- src/main/java/com/comandante/Bill.java
+++ src/main/java/com/comandante/Bill.java
@@ -2,18 +2,24 @@ package com.comandante;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.internal.Maps;
+import com.comandante.http.BillHttpClient;
 import com.comandante.http.server.BillHttpServerApplication;
 import com.comandante.http.server.BillHttpServerConfiguration;
+import com.comandante.http.server.resource.BillHttpGraph;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.dropwizard.cli.ServerCommand;
 import io.dropwizard.setup.Bootstrap;
 import net.sourceforge.argparse4j.inf.Namespace;
 
 import javax.swing.*;
+import java.io.IOException;
 
 public class Bill {
 
     public static final int DEFAULT_WIDTH = 800;
     public static final int DEFAULT_HEIGHT = 600;
+    public static final int DEFAULT_HTTP_PORT = 32224;
+    public static final int DEFAULT_HTTP_PORT_ADMIN = 32225;
 
     public static void main(String[] args) throws Exception {
         BillCommand billCommand = new BillCommand();
@@ -21,6 +27,22 @@ public class Bill {
         System.setProperty("apple.laf.useScreenMenuBar", "true");
         System.setProperty("com.apple.mrj.application.apple.menu.about.name", "Billaaaaa" + billCommand.getTitle());
         UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        BillHttpClient billHttpClient = new BillHttpClient();
+        if (isServerRunning(billHttpClient)) {
+            ObjectMapper mapper = new ObjectMapper();
+            BillHttpGraph billHttpGraph = new BillHttpGraph();
+            billHttpGraph.setHeight(billCommand.getHeight());
+            billHttpGraph.setWidth(billCommand.getWidth());
+            billHttpGraph.setTitle(billCommand.getTitle());
+            billHttpGraph.setRefreshRate(billCommand.getReloadInterval());
+            billHttpGraph.setGraphUrl(billCommand.getGraphUrl());
+            if (billCommand.getTimezone() != null) {
+                billHttpGraph.setTimezone(billCommand.getTimezone());
+            }
+            String s = mapper.writeValueAsString(billHttpGraph);
+            billHttpClient.createGraph("http://localhost:" + DEFAULT_HTTP_PORT + "/bill/create", s);
+            System.exit(0);
+        }
         BillGraph billGraph = BillGraph.createBillGraph(billCommand);
         BillGraphManager billGraphManager = new BillGraphManager();
         billGraphManager.addGraph(billGraph, billCommand.getReloadInterval());
@@ -28,5 +50,13 @@ public class Bill {
         Bootstrap bootstrap = new Bootstrap(billHttpServerApplication);
         ServerCommand<BillHttpServerConfiguration> serverConfigurationServerCommand = new ServerCommand<BillHttpServerConfiguration>(billHttpServerApplication);
         serverConfigurationServerCommand.run(bootstrap, new Namespace(Maps.<String, Object>newHashMap()));
+    }
+
+    private static boolean isServerRunning(BillHttpClient billHttpClient)  {
+        try {
+            return billHttpClient.billServerHealthCheck("http://localhost:" + DEFAULT_HTTP_PORT_ADMIN + "/healthcheck");
+        } catch (IOException e) {
+            return false;
+        }
     }
 }
--- src/main/java/com/comandante/http/BillHttpClient.java
+++ src/main/java/com/comandante/http/BillHttpClient.java
@@ -1,6 +1,7 @@
 package com.comandante.http;
 
 import com.comandante.BillGraph;
+import com.google.api.client.http.ByteArrayContent;
 import com.google.api.client.http.GenericUrl;
 import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpRequestFactory;
@@ -27,6 +28,17 @@ public class BillHttpClient {
         HttpRequest httpRequest = requestFactory.buildGetRequest(new GenericUrl(new URL(url)));
         HttpResponse execute = httpRequest.execute();
         return execute.getContent();
+    }
+
+    public boolean billServerHealthCheck(String url) throws IOException {
+        HttpRequest httpRequest = requestFactory.buildGetRequest(new GenericUrl(new URL(url)));
+        return httpRequest.execute().isSuccessStatusCode();
+    }
+
+    public boolean createGraph(String url, String json) throws IOException {
+        HttpRequest httpRequest = requestFactory.buildPostRequest(new GenericUrl(new URL(url)), ByteArrayContent.fromString("application/json", json));
+        httpRequest.getHeaders().setContentType("application/json");
+        return httpRequest.execute().isSuccessStatusCode();
     }
 }
 
