c095b59c809ee325919d7c1a67eee4e234c9ff50
Chris Kearney <chris@kearneymail.com>
2014-12-08 08:09:39 -0800

    graph duration popup menu

--- src/main/java/com/comandante/ui/BillGraphDisplayFrame.java
+++ src/main/java/com/comandante/ui/BillGraphDisplayFrame.java
@@ -1,5 +1,6 @@
 package com.comandante.ui;
 
+import com.beust.jcommander.internal.Lists;
 import com.comandante.Bill;
 import com.comandante.graph.BillGraph;
 import com.comandante.graph.BillGraphManager;
@@ -8,6 +9,8 @@ import com.comandante.graph.BillResizeEvent;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.awt.event.KeyEvent;
@@ -19,12 +22,14 @@ public class BillGraphDisplayFrame extends JFrame implements KeyListener {
 
     private BillGraphDisplayPanel billGraphDisplayPanel;
     private final BillGraphManager billGraphManager;
+    private final BillGraph billGraph;
     static GraphicsDevice device = GraphicsEnvironment
             .getLocalGraphicsEnvironment().getScreenDevices()[0];
 
     public BillGraphDisplayFrame(InputStream is, final BillGraph billGraph, final BillGraphManager billGraphManager) throws IOException {
         this.billGraphDisplayPanel = new BillGraphDisplayPanel(ImageIO.read(is));
         this.billGraphManager = billGraphManager;
+        this.billGraph = billGraph;
         this.add(billGraphDisplayPanel);
         this.setVisible(true);
         this.setTitle(billGraph.getTitle());
@@ -51,6 +56,7 @@ public class BillGraphDisplayFrame extends JFrame implements KeyListener {
             com.apple.eawt.FullScreenUtilities.setWindowCanFullScreen(this, true);
         }
         addKeyListener(this);
+        billGraphDisplayPanel.setComponentPopupMenu(new GraphEditMenu());
     }
 
     public void updateImagePanel(InputStream is, BillGraph billGraph) throws IOException {
@@ -58,6 +64,7 @@ public class BillGraphDisplayFrame extends JFrame implements KeyListener {
         this.add(newPanel);
         this.remove(billGraphDisplayPanel);
         this.billGraphDisplayPanel = newPanel;
+        this.billGraphDisplayPanel.setComponentPopupMenu(new GraphEditMenu());
         this.getContentPane().setPreferredSize(new Dimension(billGraph.getWidth(), billGraph.getHeight()));
         this.repaint();
         this.pack();
@@ -65,12 +72,12 @@ public class BillGraphDisplayFrame extends JFrame implements KeyListener {
 
     @Override
     public void keyTyped(KeyEvent e) {
-        if (e.getKeyChar() == 'f'){
+        if (e.getKeyChar() == 'f') {
             if (Bill.isOSX()) {
                 com.apple.eawt.Application.getApplication().requestToggleFullScreen(this);
             }
         }
-        if (e.getKeyChar() == 'o'){
+        if (e.getKeyChar() == 'o') {
             new BillGraphCreateFrame(billGraphManager).setVisible(true);
         }
     }
@@ -83,6 +90,43 @@ public class BillGraphDisplayFrame extends JFrame implements KeyListener {
     @Override
     public void keyReleased(KeyEvent e) {
 
+    }
+
+    class GraphEditMenu extends JPopupMenu {
+        private final JMenuItem threeHours;
+        private final JMenuItem twelveHours;
+        private final JMenuItem twentyFourHours;
+        private final JMenuItem fortyEightHours;
+        private final JMenuItem oneWeek;
+        private final JMenuItem oneMonth;
+        private final JMenuItem oneYear;
+
+        public GraphEditMenu() {
+            java.util.List<JMenuItem> durations = Lists.newArrayList();
+            durations.add(threeHours = new JMenuItem("-3h"));
+            durations.add(twelveHours = new JMenuItem("-12h"));
+            durations.add(twentyFourHours = new JMenuItem("-24h"));
+            durations.add(fortyEightHours = new JMenuItem("-48h"));
+            durations.add(oneWeek = new JMenuItem("-1weeks"));
+            durations.add(oneMonth = new JMenuItem("-1months"));
+            durations.add(oneYear = new JMenuItem("-1years"));
+
+            for (final JMenuItem item : durations) {
+                item.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        final String duration = item.getText();
+                        EventQueue.invokeLater(new Runnable() {
+                            @Override
+                            public void run() {
+                                billGraphManager.updateGraphDuration(billGraph.getId(), duration);
+                            }
+                        });
+                    }
+                });
+                add(item);
+            }
+        }
     }
 }
 
